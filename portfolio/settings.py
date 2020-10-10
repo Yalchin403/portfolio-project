a658a90e01243a4a355828e41c758706aa1f7fd3
Yalchin Mammadli <yalchin131@gmail.com>
2020-10-11 02:50:51 +0400

    from settings.py correct to from .settings.pt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,6 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 try:
-    from local_settings import *
-except:
+    from .local_settings import *
+except ImportError:
     pass
