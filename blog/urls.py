c3b456717bb7cb151c80d2359c93d0260d76fbdc
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2021-03-12 14:21:05 +0400

    url route for search bar added

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ from django.views.generic import TemplateView
 app_name='blog'
 urlpatterns = [
     path('', views.BlogView.as_view(), name='blog'),
+    path('search', views.SearchView.as_view(), name='search'),
     path('<int:blog_id>/', views.DetailView.as_view(), name='d_blog'),
     ]
