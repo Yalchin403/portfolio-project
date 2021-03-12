a04845815518cc92bb9e5bffe33609ad4727f7a7
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2021-03-12 14:20:33 +0400

    start_time field added to sort jobs based on starting date

--- jobs/models.py
+++ jobs/models.py
@@ -6,5 +6,6 @@ class Job(models.Model):
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=400)
+    start_time = models.DateTimeField(null=True)
     def __str__(self):
         return self.title
