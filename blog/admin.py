b3a930c9f80a962dabdf65778bf3869fd144d732
mrRootLog <yalchin.mammadli.std@bhos.edu.az>
2021-06-13 17:23:25 +0400

    nothing important

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Images
+from markdownx.admin import MarkdownxModelAdmin
 # Register your models her
 
 admin.site.register(Blog)
+admin.site.register(Images)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
@@ -21,3 +22,11 @@ class Blog(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+
+class Images(models.Model):
+    img_name = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.img_name
