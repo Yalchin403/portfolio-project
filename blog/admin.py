8bc750f7a476166c3cdb27c0e243df511cdc64ae
mrRootLog <yalchin.mammadli.std@bhos.edu.az>
2021-06-17 15:53:08 +0400

    mass mail notifications to the subscribers added, social share icons added to detal view

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Blog, Image, Subscription
+from .models import Blog, Image, Subscription, Notification
 # Register your models her
 
 admin.site.register(Blog)
 admin.site.register(Image)
 admin.site.register(Subscription)
+admin.site.register(Notification)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
+from django.core.mail import send_mail
+from portfolio.settings import EMAIL_HOST_USER
+from django.urls import reverse
+from django.core.mail import EmailMessage
 
 
 class Blog(models.Model):
@@ -20,10 +24,15 @@ class Blog(models.Model):
     def pretty_pub(self):
         return self.time_pub.strftime('%b %e %Y')
 
+    def get_absolute_url(self):
+        return reverse('blog:d_blog', kwargs={'slug':self.slug})
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+
 
 
 class Image(models.Model):
@@ -40,3 +49,47 @@ class Subscription(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+class Notification(models.Model):
+
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    # emails = [sub_obj.email for sub_obj in sub_objs]
+
+    subject = "New article has just been posted!"
+    
+
+    def save(self,*args, **kwargs):
+        sub_objs = Subscription.objects.all()
+        post_url = self.post.get_absolute_url()
+        for sub_obj in sub_objs:
+
+            try:
+
+                email_body =f'Hello, dear <b>{sub_obj.full_name}</b>,<br>New article on <b>"{self.post.title}"</b> has just been posted on\
+                Yalchin`s Blog!<br>See the post by following link below:<br>\
+                Link to post: https://yalchin.info{post_url}<br>Thanks for your interest!<br>Best,<br>Yalchin Mammadli'
+                
+                email = sub_obj.email
+
+                # send_mail(self.subject, email_body, EMAIL_HOST_USER, [email], fail_silently = False)
+
+                msg = EmailMessage(
+                    self.subject,
+                    email_body,
+                    EMAIL_HOST_USER,
+                    [email]
+                )
+                msg.content_subtype = "html"
+                msg.send()
+
+            except:
+                pass
+
+        return super().save(*args, **kwargs)
+
+
+    
+
+
+        
