bf46a36d92aab726e823323f7512360a30e13a0d
Yalchin Mammadli <yalchin.mammadli.std@bhos.edu.az>
2022-04-22 16:17:21 +0400

    home page design updated

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 app_name='jobs'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('forms/contact/', views.ContactView.as_view(), name='contact')
     ]
--- jobs/views.py
+++ jobs/views.py
@@ -2,8 +2,52 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 from .models import Job
-# Create your views here.
+from django.core.mail import EmailMessage
+from portfolio.settings import EMAIL_HOST_USER
+from django.contrib import messages
+
+
 class HomeView(View):
     def get(self, request):
         jobs = Job.objects.order_by('-start_time')
-        return render(request, 'jobs/home.html', {'jobs': jobs})
+        return render(request, 'jobs/new_home.html')
+
+
+class ContactView(View):
+    def get(self, request):
+        return HttpResponse("Method not allowed!")
+
+    def post(self, request):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        content = f"""
+            Hello, it's {name},
+            {message}
+            
+            Best,
+            
+            {name}
+            {email} 
+        """
+        # send email
+
+        try:
+            email_body = content
+            email = "yalchinmammadli@yalchin.info"
+
+            msg = EmailMessage(
+                subject,
+                email_body,
+                EMAIL_HOST_USER,
+                [email]
+            )
+            msg.content_subtype = "html"
+            msg.send()
+
+            return HttpResponse("Your message has been sent successfully, thanks for reaching out!", status=200)
+
+        except:
+            return HttpResponse("", status=500)
